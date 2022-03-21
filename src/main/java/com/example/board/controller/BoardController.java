510cf890883a5a5ea41450253b0db3d68a673f5f
kazuki <w124585421@yahoo.co.jp>
2022-03-21 10:23:59 +0900

    add system.properties

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -3,7 +3,6 @@ package com.example.board.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
